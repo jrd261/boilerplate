f75320448078d8dec4f1b21bf6f36050cf941ded
James Dalessio <dalessio.james@gmail.com>
2014-12-03 23:57:45 -0500

    Ready for AWS

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+var path = require('path');
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname , 'views'));
+app.set('view engine', 'jade');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'));
+	console.log('PATH IS ' + __dirname);
+});
